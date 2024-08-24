83b8dcc14ee425e01fef2cb9d8dc5ba17c69f7e0
AgentRoss <sgpandey2000@gmail.com>
2024-08-24 16:48:53 +0530

    files added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TicTacToe from './assets/components/TicTacToe/TicTacToe'
+
+const App = () => {
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  )
+}
+
+export default App
